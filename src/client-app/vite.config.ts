be926dc8dc22e880a97170384a7796fb468494db
lowern1ght <lowern1ght@yahoo.com>
2023-09-14 15:30:35 +0500

    edit Dockerfile

--- src/client-app/vite.config.ts
+++ src/client-app/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    strictPort: true,
+    port: 4044
+  },
+  preview: {
+    strictPort: true,
+    port: 80
+  }
 })
